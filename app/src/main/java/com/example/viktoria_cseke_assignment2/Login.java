6404d826028f2d9981a9fb575b1843c6d2237240
Viktoria Cseke <vcseke@gmail.com>
2020-12-07 00:58:24 +0000

    Sign in/ sign up layout done

--- app/src/main/java/com/example/viktoria_cseke_assignment2/Login.java
+++ app/src/main/java/com/example/viktoria_cseke_assignment2/Login.java
@@ -1,15 +1,18 @@
 package com.example.viktoria_cseke_assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     EditText username, password;
-    Button signin;
+    Button signin, signupnxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,8 @@ public class Login extends AppCompatActivity {
         username =(EditText) findViewById(R.id.username);
         password =(EditText) findViewById(R.id.password);
         signin = (Button) findViewById(R.id.signin);
+        signupnxt = (Button) findViewById(R.id.signupnextpg);
+
 
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +33,14 @@ public class Login extends AppCompatActivity {
 
                 DbHandler dbHandler = new DbHandler(Login.this);
                 dbHandler.GetUserByUsername(uname);
+            }
+        });
+
+        signupnxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, SignUpActivity.class));
+
             }
         });
     }
